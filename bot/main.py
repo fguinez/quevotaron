c547471630acbc20cca70ac446ff027f9067c074
fguinez <fhguinez@uc.cl>
2021-10-16 18:46:42 -0300

    :seedling: Crea clase Bot

--- bot/main.py
+++ bot/main.py
@@ -1,33 +1,44 @@
 import path
 import tweepy
+import time
 from dotenv import dotenv_values
 
 env = dotenv_values(path.this_file()+"/.env")
 
-auth = tweepy.OAuthHandler(env['API_KEY'], env['API_KEY_SECRET'])
-auth.set_access_token(env['ACCESS_TOKEN'], env['ACCESS_TOKEN_SECRET'])
-api = tweepy.API(auth)
 
 
 
+class Bot:
+    def __init__(self):
+        self.auth_twitter = tweepy.OAuthHandler(env['API_KEY'], env['API_KEY_SECRET'])
+        auth_twitter.set_access_token(env['ACCESS_TOKEN'], env['ACCESS_TOKEN_SECRET'])
+        self.api_twitter = tweepy.API(auth)
 
-def tweet_votacion(votid, media_ids):
-    # Máximo de caracteres sin link: 256
-    link = f'https://www.camara.cl/legislacion/sala_sesiones/votacion_detalle.aspx?prmIdVotacion={votid}'
-    tweet_text = f'Detalle de la votación: {link}'
-    status = api.update_status(status=tweet_text, media_ids=media_ids)
-    return status
+    def run(self, sleep=10):
+        while True:
+            nuevas_votaciones = self.get_nuevas_votaciones()
+            for votid in nuevas_votaciones:
+                # Obtiene datos de votid
+                pass
+                # Genera visualizaciones de voitid
+                #media_ids = pass
+                # Twittea votid
+                self.tweet_votacion(votid, media_ids)
+            time.sleep(sleep)
+
+    def get_nuevas_votaciones(self):
+        pass
+
+    def tweet_votacion(self, votid, media_ids):
+        # Máximo de caracteres sin link: 256
+        link = f'https://www.camara.cl/legislacion/sala_sesiones/votacion_detalle.aspx?prmIdVotacion={votid}'
+        tweet_text = f'Detalle de la votación: {link}'
+        status = self.api_twitter.update_status(status=tweet_text, media_ids=media_ids)
+        return status
 
 
 
 
 if __name__ == "__main__":
-    images = (
-        '/Users/fguinez/Documents/Visualizaciones/quevotaron/draw/img/logo.png',
-        'draw/img/logo-root1.png',
-        'draw/img/logo-root3.png',
-        '/Users/fguinez/Documents/Visualizaciones/quevotaron/draw/img/logo-root2.png',
-    )
-    #media_ids = [api.media_upload(i).media_id_string  for i in images]
-
-    #api.update_status(status="Hola mundo")
+    bot = Bot()
+    bot.run()
